650dc12573a03aed445b0d41745882096d6b5761
SPYROBO <alejandrolunaalvarez2007@gmail.com>
2024-06-12 17:01:10 -0300

    Añado css y agrego funcionalidades

--- bomba.js
+++ bomba.js
@@ -1,76 +1,83 @@
-var minutos = 0
-var segundos = 0
+var minutos = parseInt(document.getElementById("elegir_minutos").value)
+var segundos = parseInt(document.getElementById("elegir_segundos").value)
 var frenar = document.getElementById('frenar')
-let codigo = document.getElementById("codigo")
+let codigo = "";
 let mezcla = [];
 let tiempo = 0;
+let adivinar = ""
 const lobby = document.getElementById("inicio")
 const bomba = document.getElementById("bomba")
-function asignar(){
+function asignar() {
+    codigo = document.getElementById("codigo").value;
     minutos = parseInt(document.getElementById("elegir_minutos").value);
     segundos = parseInt(document.getElementById("elegir_segundos").value);
-    bomba.hidden = false
-    lobby.hidden = true
+    bomba.removeAttribute("hidden")
+    lobby.setAttribute("style", "display: none;")
     //Ejecutar cada segundo pero solo cuando lo asigne
-
     tiempo = setInterval(cargarSegundo, 1000);
-
 }
 
 //Definir y ejecutar los segundos
-function cargarSegundo(){
+function cargarSegundo() {
     var txtSegundos;
-    if(segundos < 0){
+    if (segundos < 0) {
         segundos = 59;
     }
     //Mostrar los segundos en pantalla
-    if (segundos < 10){
+    if (segundos < 10) {
         txtSegundos = `0${segundos}`;
-    }else{
+    } else {
         txtSegundos = segundos;
     }
     document.getElementById('segundos').innerHTML = txtSegundos;
     segundos--;
     cargarMinutos(segundos)
-    
+
 }
 //Definir y ejecutar minutos
-function cargarMinutos(segundos){
+function cargarMinutos(segundos) {
     var txtMinutos;
-    if(segundos == -1 && minutos !== 0){
+    if (segundos == -1 && minutos !== 0) {
         setTimeout(() => {
             minutos--;
-        },500)
-    }else if(segundos == -1 && minutos == 0){
-        alert("BOOM!");
+        }, 500)
+    } else if (segundos == -1 && minutos == 0) {
+        document.getElementById("estado").innerHTML = "BOOOOOOOOOOOOOOOOOOOOOOOOOOOM!!!!"
         clearInterval(tiempo)
+        document.getElementById("volver").removeAttribute("hidden")
     }
     //Mostrar los minutos en pantalla
-    if(minutos < 10){
+    if (minutos < 10) {
         txtMinutos = `0${minutos}`;
-    }else{
+    } else {
         txtMinutos = minutos;
     }
     document.getElementById('minutos').innerHTML = txtMinutos
-    explotar(segundos, minutos)
-}
-//Terminar el temporizador
-function explotar(segundos, minutos){
-    if(segundos == -1 && minutos == 0){
-        
-    }
-}
 
+}
 //Pausar el temporizador
-function detener(){
+function detener() {
     
-    var eliminación = setTimeout(() => {
-        clearInterval(tiempo)
-    alert("La bomba se detuvo :)")
-    }, 7000);
+    adivinar = document.getElementById("adivinar_codigo").value
+    document.getElementById("estado").innerHTML = "Probando..."
+    if (codigo === adivinar){
+        setTimeout(() => {
+            clearInterval(tiempo)
+            document.getElementById("estado").innerHTML = "Bomba detenida"
+            document.getElementById("volver").removeAttribute("hidden")
+        }, 2000);
+    }
+    else{
+        setTimeout(() => {
+            document.getElementById("estado").innerHTML = "Código Erroneo"
+        }, 2000);
+    }
+    
+    
 }
-
-//Cancelar la opción
-function cancelar(){
-    clearTimeout(eliminación)
+function volver(){
+    lobby.setAttribute("style", "display: grid")
+    bomba.hidden = true
+    document.getElementById("estado").innerHTML = ""
+    document.getElementById("volver").hidden = true
 }
